8e971734cf6d4944a57f480827c14c571b23e1a1
Nat Powning <nat@powning.org>
2025-05-30 08:25:49 -0700

    semantic-release

---
+++ release.config.js
@@ -0,0 +1,28 @@
+const config = {
+  branches: [
+    { name: "develop" },
+  ],
+
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          { type: "ci", release: "patch" },
+          { type: "chore", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "test", release: "patch" },
+          { type: "fix", release: "patch" },
+          { type: "feat", release: "minor" }
+        ],
+        preset: "angular"
+      }
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+    "@semantic-release/git"
+  ]
+}
+
+module.exports = config;
